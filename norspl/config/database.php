7838918e9e0b5793de2efd74afaaee077344e96f
Neilsen Chan <neilsen.chan88@gmail.com>
2013-08-06 14:48:41 +0800

    Add DB Connection Class

--- norspl/config/database.php
+++ norspl/config/database.php
@@ -5,3 +5,14 @@
  * @date: 7/30/13
  * @description: database.php
  */
+define('RO_DB_SCHEMA', '');
+define('RO_DB_HOST', '');
+define('RO_DB_PORT', '');
+define('RO_DB_USERNAME', '');
+define('RO_DB_PASSWORD', '');
+
+define('RW_DB_SCHEMA', '');
+define('RW_DB_HOST', '');
+define('RW_DB_PORT', '');
+define('RW_DB_USERNAME', '');
+define('RW_DB_PASSWORD', '');
